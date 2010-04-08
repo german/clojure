869bb5d2b2300d8c9ed6257bf21ec26325fca2f3
Dmitriy Samoilov <e2718281828@ya.ru>
2010-04-09 00:31:49 +0300

    added square.clj with quare root function

---
+++ square.clj
@@ -0,0 +1,20 @@
+(defn average [a b] (/ (+ a b) 2.0))
+
+(defn abs [a] (if (> a 0) a (- a)))
+
+(defn is_good_enough? [x guess] 
+  (< (abs (- (* guess guess) x)) 0.0001) 
+)
+
+(defn square-func [x guess]
+      
+    (if (is_good_enough? x guess) 
+      guess
+      (square-func x (average (/ x guess) guess))
+    )
+  
+)
+
+(defn square [x] (square-func x 1.0))
+
+(print (square 9.0))
